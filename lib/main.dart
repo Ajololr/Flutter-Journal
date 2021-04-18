5b12ca1ef18c72b07e37ff8563a9936ce4d81760
Ilya Androsov <ilya.androsau@softensity.com>
2021-04-18 21:35:21 +0300

    add firebase to ios

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,48 @@ import 'package:flutter_group_journal/models/user.modal.dart';
 import 'package:flutter_group_journal/widgets/loginSreen.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MultiProvider(
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => UserModel()),
       ChangeNotifierProvider(create: (context) => LocaleModel(context)),
     ],
-    child: App(),    
-  )
-);
+    child: App(),
+  ));
+}
 
 class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: Provider.of<LocaleModel>(context).getString("screen_login"),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Consumer<UserModel>(
-        builder: (context, user, child) {
-          return user.isLoggedIn ? Text("Logged in") : LoginsScreen();
-        },
-      ),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: Provider.of<LocaleModel>(context).getString("screen_login"),
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: Consumer<UserModel>(
+              builder: (context, user, child) {
+                return user.isLoggedIn ? Text("Logged in") : LoginsScreen();
+              },
+            ),
+          );
+        }
+
+        return Text("Loading...");
+      },
     );
   }
 }
