a6bd652330d7c3a8c8252bcc2cf5d8074350feb3
Ilya Androsov <ilya.androsau@softensity.com>
2021-04-20 14:21:26 +0300

    add cards list

--- lib/widgets/GroupScreen.dart
+++ lib/widgets/GroupScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 import 'package:flutter_group_journal/models/locale.modal.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GroupScreen extends StatefulWidget {
   GroupScreen({Key key}) : super(key: key);
@@ -11,13 +13,76 @@ class GroupScreen extends StatefulWidget {
 }
 
 class _GroupScreenState extends State<GroupScreen> {
+  CollectionReference users =
+      FirebaseFirestore.instance.collection('group mates');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Provider.of<LocaleModel>(context).getString("group")),
       ),
-      body: Text(Provider.of<LocaleModel>(context).getString("group")),
+      body: FutureBuilder<QuerySnapshot>(
+        future: users.get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<int, QueryDocumentSnapshot> data = snapshot.data.docs.asMap();
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder:(context, index) => Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        height: 220,
+                        width: double.maxFinite,
+                        child: Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          elevation: 5,
+                          child: Row(children: [
+                              Container(
+                                height: 220,
+                                child: Image.network(data[index].get("images")[0]),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+                                    Text(data[index].get("firstName")),
+                                    SizedBox(height: 10),
+                                    Text(data[index].get("lastName")),
+                                    SizedBox(height: 10),
+                                    Text(data[index].get("secondName")),
+                                    SizedBox(height: 10),
+                                    Text("Birthday: " + DateFormat(DateFormat.YEAR_NUM_MONTH_DAY).format(data[index].get("birthday").toDate()))
+                                  ]
+                                ),
+                              )
+                            ],
+                          ) 
+                        )
+                    )
+                  ),
+                )
+              ],
+            );
+          }
+
+          return Text("loading");
+        },
+      ),
     );
   }
 }
