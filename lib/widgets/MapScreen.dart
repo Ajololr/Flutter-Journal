22558188eddae85f90db5bf87a462f30b8690ab7
Ilya Androsov <ilya.androsau@softensity.com>
2021-04-25 14:19:38 +0300

    add map

--- lib/widgets/MapScreen.dart
+++ lib/widgets/MapScreen.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_group_journal/types/Student.dart';
+import 'package:flutter_group_journal/utils/firebase.dart';
+import 'package:flutter_group_journal/widgets/StudentScreen.dart';
 import 'package:provider/provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:flutter_group_journal/models/locale.modal.dart';
 
@@ -11,13 +17,60 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+  List<Student> students = [];
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(53.9006, 27.5590),
+    zoom: 8,
+  );
+
+  @override
+  void initState() {
+    loadStudents();
+    super.initState();
+  }
+
+  loadStudents() async {
+    var loaded = await FirebaseHelper.getAllStudents();
+    setState(() {
+      students = loaded;
+    });
+  }
+
+  LatLng _getLatLng(Student student) {
+    try {
+      double lat = double.parse(student.latitude);
+      double lng = double.parse(student.longitude);
+      return new LatLng(lat, lng);
+    } catch (e) {
+      return new LatLng(0, 0);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Provider.of<LocaleModel>(context).getString("map")),
       ),
-      body: Text(Provider.of<LocaleModel>(context).getString("map")),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: students.map((student) => Marker(
+          markerId: new MarkerId(student.id),
+          position: _getLatLng(student),
+          infoWindow: InfoWindow(
+            title: "${student.firstName} ${student.lastName}", 
+            onTap: () => Navigator.push(context, MaterialPageRoute(
+            builder: (BuildContext context) =>
+                StudentSceen(student: student)))
+          ),
+        )).toSet(),
+      ),
     );
   }
 }
